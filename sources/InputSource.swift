5ab4419940c894cb5c23cf4d9e11a126418188f3
LEO Yoon-Tsaw <leoyoontsaw@icloud.com>
2024-05-24 21:55:16 -0400

    Update IMKTextInput when possible, code gardening

--- sources/InputSource.swift
+++ sources/InputSource.swift
@@ -17,7 +17,7 @@ final class SquirrelInstaller {
   private lazy var inputSources: [String: TISInputSource] = {
     var inputSources = [String: TISInputSource]()
     var matchingSources = [InputMode: TISInputSource]()
-    let sourceList = TISCreateInputSourceList(nil, true).takeUnretainedValue() as! [TISInputSource]
+    let sourceList = TISCreateInputSourceList(nil, true).takeRetainedValue() as! [TISInputSource]
     for inputSource in sourceList {
       let sourceIDRef = TISGetInputSourceProperty(inputSource, kTISPropertyInputSourceID)
       guard let sourceID = unsafeBitCast(sourceIDRef, to: CFString?.self) as String? else { continue }
